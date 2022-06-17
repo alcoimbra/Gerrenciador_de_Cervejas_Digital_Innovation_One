b2ad45dd90f22e09c7ddea545edec7b2ef929c07
Alan Coimbra <algcoimbra01@gmail.com>
2022-06-17 17:43:42 -0300

    Iniciando o Projeto e Adicionando as Dependencias no POM

---
+++ src/main/java/br/com/gerenciamentoDeCervejas/GerenciamentoDeCervejasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamentoDeCervejas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCervejasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeCervejasApplication.class, args);
+	}
+
+}
